e67a138b219541b9d9c4cf9d1e92f4bcb275ac14
vivek-creator <tripathivivek898@gmail.com>
2024-03-11 14:46:16 +0530

    final push

---
+++ server/src/main/java/com/indeed/server/constants/APIConstants.java
@@ -0,0 +1,9 @@
+package com.indeed.server.constants;
+
+public class APIConstants {
+
+    public static final String SAVE_POST = "/post";
+
+    public static final String GET_ALL_POSTS ="/posts";
+
+}
